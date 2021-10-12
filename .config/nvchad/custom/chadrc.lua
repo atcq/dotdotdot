148ef82df41374c6f80936270e129300be7fff82
atcq <atcq@atcq.de>
2021-10-12 23:50:50 +0200

    nvchad config updates

--- .config/nvchad/custom/chadrc.lua
+++ .config/nvchad/custom/chadrc.lua
@@ -1,30 +1,42 @@
--- IMPORTANT NOTE : This is the user config, can be edited. Will be preserved if updated with internal updater
--- This file is for NvChad options & tools, custom settings are split between here and 'lua/custom/init.lua'
+-- IMPORTANT NOTE : This is the user config, can be edited. Will be preserved if updated
+-- with internal updater
+-- This file is for NvChad options & tools, custom settings are split between here and
+-- 'lua/custom/init.lua'
 
 local M = {}
 M.options, M.ui, M.mappings, M.plugins = {}, {}, {}, {}
 
--- NOTE: To use this, make a copy with `cp example_chadrc.lua chadrc.lua`
 
---------------------------------------------------------------------
+M.options = {
+   -- enable relative line numbers
+   relativenumber = true,
+   -- add .rasi file extension to the FileType autocmd
+   vim.cmd "autocmd BufReadPost *.rasi setl ft=rasi",
+}
 
--- To use this file, copy the structure of `core/default_config.lua`,
--- examples of setting relative number & changing theme:
-
--- M.options = {
---    relativenumber = true,
--- }
-
--- M.ui = {
---   theme = "nord"
--- }
+M.ui = {
+   theme = "chadracula"
+   }
 
 -- NvChad included plugin options & overrides
 M.plugins = {
+   status = {
+     dashboard = true,
+     colorizer = true,
+     telescope_media = true,
+   },
    options = {
       lspconfig = {
       -- path of file containing setups of different lsps (ex : "custom.plugins.lspconfig"), read the docs for more info
          setup_lspconf = "custom.plugins.lspconfig",
+      },
+      statusline = {
+         shown = {
+            "help",
+            "dashboard",
+         },
+         -- default, round, slant, block, arrow
+         style = "arrow",
       },
    },
    -- To change the Packer `config` of a plugin that comes with NvChad,
@@ -33,7 +45,9 @@ M.plugins = {
    -- this string will be called in a `require`
    --              use "(custom.configs).my_func()" to call a function
    --              use "custom.blankline" to call a file
-   default_plugin_config_replace = {},
+   default_plugin_config_replace = {
+      nvim_colorizer = "(custom.plugins.colorizer).colorizer()",
+   },
 }
 
 return M
